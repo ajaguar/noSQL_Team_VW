d8cc3f48b2a49734c3c86217c947b256726ba5eb
a_JAguar <weber.jonas@gmail.com>
2015-12-11 09:49:23 +0100

    clean workspace

--- webserver.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var port = 8080;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hallo Welt!');
-});
-server.listen(port);
-
-console.log('Der Server ist erreichbar unter http://127.0.0.1:' + port + '/');
