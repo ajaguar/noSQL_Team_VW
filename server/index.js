52e50b23b204398492cd6522a4aba9927d0c3fbf
Joshua Vecsei <j.vecsei@gmx.de>
2015-12-12 17:21:08 +0100

    rest services for elastic search, add / search document

--- server/index.js
+++ server/index.js
@@ -1,18 +1,102 @@
 'use strict';
 
 var express = require('express');
+var fs = require("fs");
+var multer = require("multer");
+var elasticsearch = require('elasticsearch');
 var app = express();
-
-//app.get('/', function (req, res) {
-//  res.send('Hello World!');
-//});
-app.use(express.static(__dirname + '/../client'));
-
-
-var server = app.listen(process.env.npm_package_config_server_port || 3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+var upload = multer({
+    dest: 'uploads/'
+})
+var esClient = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
 });
 
+
+app.use(express.static(__dirname + '/../client'));
+
+app.post('/document', upload.single('document'), function (req, res) {
+    fs.readFile(req.file.path, "utf-8", function (err, data) {
+        if (err) throw err;
+        console.log(req.file);
+        console.log(data);
+        esClient.create({
+            "index": 'file',
+            "type": 'document',
+            "body": {
+                "title": req.file.name,
+                "content": data,
+                "published_at": new Date()
+            }
+        }, function (error, response) {
+            if (error)
+                throw error;
+            fs.unlink(req.file.path, function (err) {
+                if (err) throw err;
+            });
+        });
+    });
+    res.send("yes!");
+});
+
+app.get("/document", function (req, res) {
+    var search = req.query.search;
+    if(!search) 
+        throw "no search value given";
+    esClient.search({
+        "body": {
+            "query": {
+                "match": {
+                    "content": search
+                }
+            }
+        },
+        "index": "file",
+        "type": "document"
+    }, function (err, response) {
+        if (err)
+            throw err;
+        
+        res.send(response);
+    })
+});
+
+
+initIndexIfNotExists();
+
+var server = app.listen(process.env.npm_package_config_server_port || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+function initIndexIfNotExists() {
+    esClient.indices.delete({
+        index: "file"
+    }, function () {
+        console.log("creating index");
+        esClient.indices.create({
+            index: "file"
+        }, function () {
+            esClient.indices.putMapping({
+                "body": {
+                    "properties": {
+                        "title": {
+                            "type": "string"
+                        },
+                        "content": {
+                            "type": "string"
+                        },
+                        "published_at": {
+                            "type": "date"
+                        }
+                    }
+                },
+                "index": "file",
+                "type": "document"
+            });
+        });
+    });
+}
