68a2acade2ef217a293f6b3edcd59cbd1869db9b
a_JAguar <weber.jonas@gmail.com>
2015-12-12 21:18:38 +0100

    jshint anpassungen

--- server/index.js
+++ server/index.js
@@ -20,47 +20,53 @@ app.use(express.static(__dirname + '/../client'));
 app.use('/bower_components', express.static(__dirname + '../../bower_components'));
 
 app.post('/document', upload.single('document'), function (req, res) {
-    fs.readFile(req.file.path, "utf-8", function (err, data) {
-        if (err) throw err;
+    fs.readFile(req.file.path, 'utf-8', function (err, data) {
+        if (err) {
+            throw err;
+        }
         console.log(req.file);
         console.log(data);
         esClient.create({
-            "index": 'file',
-            "type": 'document',
-            "body": {
-                "title": req.file.name,
-                "content": data,
-                "published_at": new Date()
+            'index': 'file',
+            'type': 'document',
+            'body': {
+                'title': req.file.name,
+                'content': data,
+                'published_at': new Date()
             }
         }, function (error, response) {
-            if (error)
+            if (error) {
                 throw error;
+            }
             fs.unlink(req.file.path, function (err) {
-                if (err) throw err;
+                if (err) {
+                    throw err;
+                }
             });
         });
     });
     res.send('yes!');
 });
 
-app.get("/document", function (req, res) {
+app.get('/document', function (req, res) {
     var search = req.query.search;
-    if (!search)
+    if (!search) {
         throw 'no search value given';
+    }
     esClient.search({
-        "body": {
-            "query": {
-                "match": {
-                    "content": search
+        'body': {
+            'query': {
+                'match': {
+                    'content': search
                 }
             }
         },
         'index': 'file',
         'type': 'document'
     }, function (err, response) {
-        if (err)
+        if (err) {
             throw err;
-
+        }
         res.send(response);
     });
 });
@@ -77,28 +83,28 @@ var server = app.listen(process.env.npm_package_config_server_port || 3000, func
 
 function initIndexIfNotExists() {
     esClient.indices.delete({
-        index: "file"
+        index: 'file'
     }, function () {
-        console.log("creating index");
+        console.log('creating index');
         esClient.indices.create({
-            index: "file"
+            index: 'file'
         }, function () {
             esClient.indices.putMapping({
-                "body": {
-                    "properties": {
-                        "title": {
-                            "type": "string"
+                'body': {
+                    'properties': {
+                        'title': {
+                            'type': 'string'
                         },
-                        "content": {
-                            "type": "string"
+                        'content': {
+                            'type': 'string'
                         },
-                        "published_at": {
-                            "type": "date"
+                        'published_at': {
+                            'type': 'date'
                         }
                     }
                 },
-                "index": "file",
-                "type": "document"
+                'index': 'file',
+                'type': 'document'
             });
         });
     });
