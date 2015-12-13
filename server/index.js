9308a10ca637ca5b38ce387c3c631dacadc0a32c
Joshua Vecsei <j.vecsei@gmx.de>
2015-12-13 19:29:28 +0100

    fixes

--- server/index.js
+++ server/index.js
@@ -6,3 +6,9 @@ var controller = require('./controller')(app);
 
 app.use(express.static(__dirname + '/../client'));
 app.use('/bower_components', express.static(__dirname + '../../bower_components'));
+
+var server = app.listen(process.env.npm_package_config_server_port || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
