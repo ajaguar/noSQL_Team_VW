5990200db44fbac785d3a1ff724afd9c9a39607a
Joshua Vecsei <j.vecsei@gmx.de>
2015-12-19 02:57:51 +0100

    custom scoring function implemented

--- server/controller.js
+++ server/controller.js
@@ -49,49 +49,51 @@ module.exports = function (app) {
     });
 
     app.get('/document', function (req, res) {
-        var search = req.query.search;
+        //remove all non alpha-numeric characters for security reasons regarding the inline scripting
+        var search = req.query.search.replace(/[^\w\s\u00C0-\u017F]+/g, '').toLowerCase();
         if (!search) {
             res.status(400).send('no search value sent');
         } else {
             esClient.search({
-                'body': {
-                    'query': {
-                        'bool': {
-                            should: [
-                                {
+                    'body': {
+                        'query': {
+                            'function_score': {
+                                'query': {
                                     'match': {
                                         'content': search
                                     }
                                 },
-                                {
-                                    'wildcard': {
-                                        'title': '*'+search+'*'
-                                    }
+                                'boost_mode': 'replace',
+                                'functions': [
+                                    {
+                                        'script_score': {
+                                            'script': '_index["content"]["' + search + '"].tf() + _index["title"]["' + search + '"].tf()'
+                                        }
                                 }
                             ]
-                        }
-
-                    },
-                    'highlight': {
-                        'fields': {
-                            'content': {
-                                'fragment_size': 50
-                            },
-                            'title': {
-                                fragment_size: 0
                             }
-                        }
+                        },
+                        'highlight': {
+                            'fields': {
+                                'content': {
+                                    'fragment_size': 50
+                                },
+                                'title': {
+                                    fragment_size: 0
+                                }
+                            }
+                        },
+                        fields: ['title']
                     },
-                    fields: ['title']
+                    'index': 'file',
+                    'type': 'document'
                 },
-                'index': 'file',
-                'type': 'document'
-            }, function (err, response) {
-                if (err) {
-                    throw err;
-                }
-                res.send(response.hits);
-            });
+                function (err, response) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.send(response.hits);
+                });
         }
     });
 
@@ -108,10 +110,11 @@ module.exports = function (app) {
                         'properties': {
                             'title': {
                                 'type': 'string',
-                                'index': 'not_analyzed'
+                                'index': 'not_analyzed',
+                                'index_options': 'freqs'
                             },
                             'content': {
-                                'type': 'string'
+                                'type': 'string',
                             },
                             'published_at': {
                                 'type': 'date'
